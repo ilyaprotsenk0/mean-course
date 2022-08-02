58d8f3228a9cf0bb81d5ad91cf882710829a915f
Ilya Protsenko <iliafreshfish@gmail.com>
2022-08-02 11:02:11 +0300

    Project before authentefication chapter

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/models/post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  isLoading = false;
+  public totalPosts = 11;
+  public postsPerPage = 1;
+  public currentPage = 1;
+  public pageSizeOptions = [1, 2, 5, 10];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    this.postsSub = this.postsService
+      .getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.isLoading = false;
+        this.posts = posts;
+      });
+  }
+
+  onPageChange(pageData: PageEvent) {
+    this.isLoading = true;
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+  }
+
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
